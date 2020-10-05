be0084b7dfcd1e80bfa105a2566e5f4eec53ed0a
Nikolaj Lozitski <Soullnik96@gmail.com>
2020-10-05 13:15:52 +0300

    feat: decoder complited

--- src/index.js
+++ src/index.js
@@ -38,7 +38,38 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let morseExpr = '';
+    let resultExpr = '';
+    for (let i = 0; i < expr.length/2; i++) {
+        let temp = expr.slice(i*2, i*2+2);
+        switch (temp) {
+            case "00":
+                morseExpr += " ";
+                break
+            case "10":
+                morseExpr += ".";
+                break
+            case "11":
+                morseExpr += "-";
+                break
+            case "**":
+                morseExpr += " ";
+                break
+        }
+    }
+
+    for (let i = 0; i < morseExpr.length/5; i++) {
+        let temp = morseExpr.slice(i*5, i*5+5);
+        if (temp === "     ") {
+            resultExpr += " ";
+        } else {
+            temp = temp.trim();
+            resultExpr += MORSE_TABLE[temp];
+        }
+    }
+    return resultExpr;
+
+
 }
 
 module.exports = {
